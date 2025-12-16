d43fdbfc92cff837811a886970593a82efa607ff
NexusDeco <denis.andr0101@gmail.com>
2025-12-16 21:34:42 +0300

    Update base_student.h

--- lab7/base_student.h
+++ lab7/base_student.h
@@ -20,8 +20,6 @@ public:
     BaseStudent(const char* name, int course, int group, int recordBook);
     BaseStudent(const BaseStudent& other);
     virtual ~BaseStudent();
-    
-    BaseStudent& operator=(const BaseStudent& other) = delete;
 
     int getStudentID() const { return studentID; }
     const char* getFullName() const { return fullName; }
